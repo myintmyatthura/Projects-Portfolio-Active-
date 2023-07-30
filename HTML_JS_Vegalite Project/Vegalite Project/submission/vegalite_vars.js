9055e95562dc01050427705a7f7004a4b9c73797
myintmyatthura <46368692+myintmyatthura@users.noreply.github.com>
2023-07-30 21:29:23 +0800

    Add files via upload

---
+++ HTML_JS_Vegalite Project/Vegalite Project/submission/vegalite_vars.js
@@ -0,0 +1,20 @@
+var vg_1 = "hunger_map.json"; 
+vegaEmbed("#map", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "stunting.json";
+vegaEmbed("#bar_graph", vg_2).then(function(result){
+
+}).catch(console.error)
+
+var vg_3 = "index_stunt.json";
+vegaEmbed("#scatter", vg_3).then(function(result){
+
+}).catch(console.error)
+
+var vg_4 = "index_2000.json";
+vegaEmbed("#map_3", vg_4).then(function(result){
+
+}).catch(console.error)
+
